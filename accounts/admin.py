dd51e5103850d172bacf20b7ee568e9cc459b673
Dmitry Parshukov <dimer3799@yandex.ru>
2021-02-01 12:31:38 +0600

    create table database

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer, Pruduct, Order
+
+admin.site.register(Customer)
+admin.site.register(Pruduct)
+admin.site.register(Order)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    # Клиенты
+    name = models.CharField(max_length=200, null = True, verbose_name = 'ФИО')
+    phone = models.CharField(max_length=200, null = True, verbose_name = 'Телефон')
+    email = models.CharField(max_length=200, null = True, verbose_name = 'Эл. почта')
+    date_created = models.DateTimeField(auto_now_add=True, null = True, verbose_name = 'Дата')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Клиенты'
+        verbose_name = 'Клиент'
+
+class Pruduct(models.Model):
+    # Продукция
+    CATEGORY = (
+        ('Для дома', 'Для дома'),
+        ('Для улицы', 'Для улицы'),
+    )
+    name = models.CharField(max_length=200, null = True, verbose_name = 'Наименования')
+    price = models.FloatField(null = True, verbose_name = 'Цена')
+    category = models.CharField(max_length=200, null = True, choices = CATEGORY, verbose_name = 'Категория')
+    description = models.CharField(max_length=200, null = True, verbose_name = 'Описание')
+    date_created = models.DateTimeField(auto_now_add=True, null = True, verbose_name = 'Дата')
+
+    class Meta:
+        verbose_name_plural = 'Продукция'
+        verbose_name = 'Продукция'
+
+class Order(models.Model):
+    #Заказы
+    STATUS = (
+        ('В ожидании', 'В ожидании'),
+        ('Готово к доставке', 'Готово к доставке'),
+        ('Доставлено', 'Доставлено'),
+    )
+    # customer = 
+    # product = 
+    date_created = models.DateTimeField(auto_now_add=True, null = True, verbose_name = 'Дата')
+    status = models.CharField(max_length = 200, null = True, choices = STATUS, verbose_name = 'Статус заказа')
+
+    class Meta:
+        verbose_name_plural = 'Заказы'
+        verbose_name = 'Заказ'
+    
