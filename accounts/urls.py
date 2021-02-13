2157f444e7f70dc686209a9577fb94f7e504adfa
Dmitry Parshukov <dimer3799@yandex.ru>
2021-02-13 20:17:27 +0600

    update language, add templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home, products, customer
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('products', products, name='products'),
     path('customer', customer, name='customer'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Customer, Pruduct, Order
 
 def home(request):
-	return render(request, 'accounts/dashboard.html')
+    order = Order.objects.all()
+    customer = Customer.objects.all()
+    # Общее количество клиентов
+    total_customers = customer.count()
+    # Общее количество заказов
+    total_order = order.count()
+    # Выбираем заказы с фильтром -доставлено
+    delivered = order.filter(status = 'Доставлено').count()
+    # Выбираем заказы с фильтром -в ожидании
+    pending = order.filter(status = 'В ожидании').count()
+
+    context = {'orders': order, 'customers': customer , 'total_customers': total_customers, 
+               'total_order': total_order, 'delivered': delivered, 'pending': pending}
+    
+    return render(request, 'accounts/dashboard.html', context)
     
 def products(request):
-    return render(request, 'accounts/products.html')
+    # Выбрать из базы всю пролукцию
+    products = Pruduct.objects.all()
+    # {'pruducts':products} - передать в шаблон контекст для вывода
+    return render(request, 'accounts/products.html', {'products':products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
