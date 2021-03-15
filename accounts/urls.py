583aec0579ef44b03e380010c46ff23be7e6859d
Dmitry Parshukov <dimer3799@yandex.ru>
2021-03-15 11:12:48 +0600

    add decorations unauthenticated

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import home, products, customer, createOrders, updateOrder, deleteOrder, registerPage, loginPage, logoutUser
+from .views import home, products, customer, createOrders, updateOrder, deleteOrder, registerPage, loginPage, logoutUser, userPage
 
 urlpatterns = [
     path('', home, name = 'home'),
     path('register', registerPage, name = 'register'),
     path('login', loginPage, name = 'login'),
     path('logout/', logoutUser, name = 'logout'),
+    path('user/', userPage, name = 'user-page'),
     path('products', products, name = 'products'),
     # <str:pk> передаем строковое занчение в pk
     path('customer/<str:pk>', customer, name = 'customer'),
--- accounts/views.py
+++ accounts/views.py
@@ -7,46 +7,40 @@ from .filters import OrderFilter
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .decorations import unauthenticated_user
 
 from django.contrib.auth.decorators import login_required
 
-
+@unauthenticated_user
 def registerPage(request):
-    # Если пользователь уже авторизован
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user= form.cleaned_data.get('username')
-                messages.success(request, 'Аккаунт успешно создан,' + user )
-                return redirect('login')
-        context = {'form': form}
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user= form.cleaned_data.get('username')
+            messages.success(request, 'Аккаунт успешно создан,' + user )
+            return redirect('login')
+    context = {'form': form}
+    return render(request, 'accounts/register.html' ,context)
 
-        return render(request, 'accounts/register.html' ,context)
-
+@unauthenticated_user
 def loginPage(request):
-    if request.user.is_authenticated:
-            return redirect('home')
-    else:
-        if request.method == 'POST':
-            # Из запросв получаем логин и пароль
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+    if request.method == 'POST':
+        # Из запросв получаем логин и пароль
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-            # Делаем аунтификацию
-            user = authenticate(request, username = username, password = password)
-            # Если пользователь существует в системе
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.info(request, 'Логин или пароль неверный')
-        context = {}
-        return render(request, 'accounts/login.html', context)
+        # Делаем аунтификацию
+        user = authenticate(request, username = username, password = password)
+        # Если пользователь существует в системе
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Логин или пароль неверный')
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 def logoutUser(request):
     logout(request)
@@ -70,6 +64,10 @@ def home(request):
                'total_order': total_order, 'delivered': delivered, 'pending': pending}
     
     return render(request, 'accounts/dashboard.html', context)
+
+def userPage(request):
+    context = {}
+    return render(request, 'accounts/user.html', context)
 
 # Декоратор перенаправляет на страницу login если пользователь не авторизован
 @login_required(login_url='login')
